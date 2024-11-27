352e03705790610d92494e2759a1b8ace3c198e7
h-ryushin <h.ryushin0410@gmail.com>
2024-11-27 15:54:57 +0900

    スタイルを主に変更

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import soundjump from "../img/soundjump.png";
+import realbaseball from "../img/realbaseball.png"
 
 function ProjectCard() {
   // プロジェクト情報の配列
   const projects = [
     { name: "SoundJump", tool: "Unity, C#", image: soundjump, link: 'https://unityroom.com/games/soundjump' },
-    { name: "React App", tool: "React, JavaScript", image: "react.png", link: 'https://reactjs.org/' },
+    { name: "RealBaseBall", tool: "Unity, C#", image: realbaseball, link: 'https://reactjs.org/' },
   ];
 
   return (
@@ -15,7 +16,7 @@ function ProjectCard() {
         <div key={index} className="project-card">
           <div className="projectImage">
             <a href={project.link}>
-              <img src={projects[0].image} alt={`${project.name}の写真`} />
+              <img src={project.image} alt={`${project.name}の写真`} />
             </a>
           </div>
           {/* プロジェクト名 */}
