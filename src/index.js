63400d572ddd4ea17859ffa0bf4e277e9299e106
h-ryushin <h.ryushin0410@gmail.com>
2024-11-28 00:58:54 +0900

    リンクをわけることができた

--- src/index.js
+++ src/index.js
@@ -4,15 +4,32 @@ import Top from './Top';
 import ProjectCard from './Components/ProjectCard';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import About from './Components/About';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header/>
-    <Top />
-    <ProjectCard />
-    <Footer />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={
+          <>
+            <Top />
+            <ProjectCard />
+          </>
+        } />
+        <Route path='/about' element={
+          <>
+            <Top />
+            <About />
+          </>
+        } />
+      </Routes>
+      <Footer />
+
+    </BrowserRouter>
   </React.StrictMode>
 );
 
